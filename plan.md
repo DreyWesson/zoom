5063303370179c23c6b776c5370bbcd0ea8a3952
dreyWesson <dreyWesson@gmail.com>
2020-12-10 20:17:43 +0100

    first commit

---
+++ plan.md
@@ -0,0 +1,10 @@
+# Todos
+
+- Initialize our NodeJS project
+- Initialize our first view
+- Create a room ID
+- Add ability to view our own video & others video
+- Add styling
+- Add message capabilities
+- Add mute button
+- Add stop video button
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+
+// app config
+const port = process.env.PORT || 3030,
+  io = require("socket.io")(server);
+
+// PEER js
+const { ExpressPeerServer } = require("peer"),
+  peerServer = ExpressPeerServer(server, {
+    debug: true,
+  });
+
+// middlewares
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/peerjs", peerServer);
+
+// DB config
+
+// api routes
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (msg) => {
+      io.to(roomId).emit("createMessage", msg);
+    });
+  });
+});
+// Listener
+server.listen(port, () => console.log(`listening on http://localhost:${port}`));
